f20c4dd2663233fd2b40a6e788aea93fdc75b840
Rithsagea Aquadom <rithsagea@gmail.com>
2021-06-04 10:30:03 -0400

    more skeleton code

--- ChessEngine/src/chess/move/CastleMove.java
+++ ChessEngine/src/chess/move/CastleMove.java
@@ -1,13 +1,34 @@
 package chess.move;
 
 import chess.Board;
+import chess.ColorType;
 import chess.Location;
 
 public class CastleMove extends Move {
 
-	public CastleMove(Board board, Location start, Location end) {
+	public CastleMove(Board board, Location start, Location end, boolean longCastle) {
 		super(board, start, end, false);
-		// TODO Auto-generated constructor stub
+		
+		//check whole row cuz pain here
+		
+		if(piece.getColor() == ColorType.WHITE) {
+			if(longCastle) {
+				checkSquare(start.add(0, -1));
+			} else {
+				checkSquare(start.add(0, 1));
+			}
+		} else {
+			if(longCastle) {
+				checkSquare(start.add(0, 1));
+			} else {
+				checkSquare(start.add(0, -1));
+			}
+		}
+	}
+	
+	@Override
+	public void executeMove(Board board) {
+		//pain
 	}
 
 }
